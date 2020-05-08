fa4f0ce1d3e7eea266fe7362c84fa7db3ce7d298
Jing Jie Wang <s10198179@connect.np.edu.sg>
2020-05-08 23:05:07 +0800

    layout

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -1,14 +1,24 @@
 package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.media.MediaPlayer;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MediaPlayer sound;
+    private VideoView video;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        video = (VideoView) findViewById(R.id.videoView);
+        sound = MediaPlayer.create(this,R.raw.baby_laughing);
+        sound.start();
+        video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid));
+        video.start();
     }
 }
